59a7b0a3016b91cc629d290b5291691f82efdae1
Lucas Lemanski <lucaslemanski.ofc@gmail.com>
2022-11-24 23:11:47 -0300

    Organização dos Programas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-function troca_tema(){
-    var element = document.body;
-    element.classList.toggle("light_mode");
-
+function troca_tema()
+{
+    var tema = document.body;
+    tema.classList.toggle("light_mode");
+}
+function seleciona_parte()
+{
+    var selecionar = document.conteudo_item;
+    selecionar.classList.toggle(".selecao");
 }
