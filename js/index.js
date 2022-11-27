ae1fbbf80dd4bc56c005367af86e1da1c789ec6b
Lucas Lemanski <lucaslemanski.ofc@gmail.com>
2022-11-27 19:11:17 -0300

    correção do erro das imagens

--- js/index.js
+++ js/index.js
@@ -3,8 +3,3 @@ function troca_tema()
     var tema = document.body;
     tema.classList.toggle("light_mode");
 }
-function seleciona_parte()
-{
-    var selecionar = document.conteudo_item;
-    selecionar.classList.toggle(".selecao");
-}
