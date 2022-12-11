c9eba6c96db565fc20eaa795a37647d633035ce1
lemanski <lucaslemanski.ofc@gmail.com>
2022-12-11 07:31:56 -0300

    Atualizações finais e remoção da seleção

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ function troca_tema()
     var tema = document.body;
     tema.classList.toggle("light_mode");
 }
+
+function selecionar_btn()
+{
+    var selecionou = document.body;
+    selecionou.classList.toggle("selecao");
+}
